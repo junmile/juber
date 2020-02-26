e48aaffb79ea007f67757298123a8fe4de2cf4e9
junmile <designeryjun@gmail.com>
2020-02-26 19:32:14 +0900

    #1.11

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -1,15 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { ConnectionOptions } from 'typeorm';
-
 const connectionOptions: ConnectionOptions = {
   type: 'postgres',
   database: 'juber',
   synchronize: true,
   logging: true,
   entities: ['entities/**/*.*'],
-  host: process.env.DB_ENDPOINT || 'localhost',
+  host: process.env.DB_ENDPOINT,
   port: 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'dlwpdksgo3'
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
 };
 
 export default connectionOptions;
