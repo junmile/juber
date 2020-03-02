f645b24bb2a90b96774076ac798068669397ee46
junmile <designeryjun@gmail.com>
2020-03-02 18:10:58 +0900

    #1.29

---
+++ src/api/User/FacebookConnect/FacebookConnect.resolvers.ts
@@ -0,0 +1,43 @@
+import { Resolvers } from 'src/types/resolvers';
+import {
+  FacebookConnectResponse,
+  FacebookConnectMutationArgs
+} from '../../../types/graph';
+import User from 'src/entities/User';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    FacebookConnect: async (
+      _,
+      args: FacebookConnectMutationArgs
+    ): Promise<FacebookConnectResponse> => {
+      const { fbId } = args;
+      try {
+        const existingUser = await User.findOne({ fbId });
+        if (existingUser) {
+          return {
+            ok: true,
+            error: null,
+            token: 'Coming soon'
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
+      try {
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
