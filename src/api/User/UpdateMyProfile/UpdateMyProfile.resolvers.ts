3af62ae6117bb19dff8346043373a0dc719d17be
junmile <designeryjun@gmail.com>
2020-03-13 20:15:23 +0900

    #1.57

--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -1,17 +1,38 @@
-import { Resolvers } from 'src/types/resolvers';
-import privateResolver from 'src/api/utils/privateResolver';
+import { Resolvers } from '../../../types/resolvers';
+import privateResolver from '../../../api/utils/privateResolver';
 import {
   UpdateMyProfileResponse,
   UpdateMyProfileMutationArgs
 } from 'src/types/graph';
-import User from 'src/entities/User';
+import User from '../../../entities/User';
 
 const resolvers: Resolvers = {
   Mutation: {
     UpdateMyProfile: privateResolver(
-      async (_, args: UpdateMyProfileMutationArgs, { req }) => {
+      async (
+        _,
+        args: UpdateMyProfileMutationArgs,
+        { req }
+      ): Promise<UpdateMyProfileResponse> => {
         const user: User = req.user;
-        await User.update({ id: user.id }, { ...args });
+        const notNull = {};
+        Object.keys(args).forEach((key) => {
+          if (args[key] !== null) {
+            notNull[key] = args[key];
+          }
+        });
+        try {
+          await User.update({ id: user.id }, { ...notNull });
+          return {
+            ok: true,
+            error: null
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message
+          };
+        }
       }
     )
   }
