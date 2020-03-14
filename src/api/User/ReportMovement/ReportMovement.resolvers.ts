62e9f0604d87677dd8afa22fd4d9538be52be854
junmile <designeryjun@gmail.com>
2020-03-14 19:30:59 +0900

    #1.59

--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -1,28 +1,23 @@
-import { Resolvers } from '../../../types/resolvers';
-import privateResolver from '../../../api/utils/privateResolver';
+import { Resolvers } from 'src/types/resolvers';
+import privateResolver from 'src/api/utils/privateResolver';
 import {
-  UpdateMyProfileResponse,
-  UpdateMyProfileMutationArgs
-} from 'src/types/graph';
-import User from '../../../entities/User';
+  ReportMovementMutationArgs,
+  ReportMovementResponse
+} from '../../../types/graph';
+import User from 'src/entities/User';
 import cleanNullArgs from 'src/api/utils/cleanNullArgs';
 
 const resolvers: Resolvers = {
   Mutation: {
-    UpdateMyProfile: privateResolver(
+    ReportMovement: privateResolver(
       async (
         _,
-        args: UpdateMyProfileMutationArgs,
+        args: ReportMovementMutationArgs,
         { req }
-      ): Promise<UpdateMyProfileResponse> => {
+      ): Promise<ReportMovementResponse> => {
         const user: User = req.user;
         const notNull = cleanNullArgs(args);
-
         try {
-          if (args.password !== null) {
-            user.password = args.password;
-            user.save();
-          }
           await User.update({ id: user.id }, { ...notNull });
           return {
             ok: true,
@@ -38,5 +33,3 @@ const resolvers: Resolvers = {
     )
   }
 };
-
-export default resolvers;
--- src/api/User/ToggleDrivingMode/ToggleDrivingMode.resolvers.ts
+++ src/api/User/ToggleDrivingMode/ToggleDrivingMode.resolvers.ts
@@ -1,6 +1,7 @@
 import { Resolvers } from 'src/types/resolvers';
 import privateResolver from 'src/api/utils/privateResolver';
 import User from 'src/entities/User';
+import { ToggleDrivingModeResponse } from 'src/types/graph';
 
 const resolvers: Resolvers = {
   Mutation: {
