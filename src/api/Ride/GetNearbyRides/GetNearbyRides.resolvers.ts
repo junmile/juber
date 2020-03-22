93693a6a0f36458079311450a704544426845646
junmile <designeryjun@gmail.com>
2020-03-22 20:17:20 +0900

    #1.73

---
+++ src/api/Ride/GetNearbyRides/GetNearbyRides.resolvers.ts
@@ -0,0 +1,46 @@
+import { Resolvers } from '../../../types/resolvers';
+import privateResolver from '../../utils/privateResolver';
+import User from '../../../entities/User';
+import { GetNearbyRidesResponse } from '../../../types/graph';
+import { Between } from 'typeorm';
+import Ride from '../../../entities/Ride';
+
+const resolvers: Resolvers = {
+  Query: {
+    GetNearbyRides: privateResolver(
+      async (_, __, { req }): Promise<GetNearbyRidesResponse> => {
+        const user: User = req.user;
+        if (user.isDriving) {
+          const { lastLat, lastLng } = user;
+
+          try {
+            const rides: any = await Ride.find({
+              status: 'REQUESTING',
+              pickUpLat: Between(lastLat - 0.05, lastLng + 0.05),
+              pickUpLng: Between(lastLng - 0.05, lastLng + 0.05)
+            });
+            return {
+              ok: true,
+              error: null,
+              rides
+            };
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message,
+              rides: null
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: 'You are not a driver',
+            rides: null
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
