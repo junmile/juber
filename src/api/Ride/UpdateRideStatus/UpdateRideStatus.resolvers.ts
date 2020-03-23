7a942d79ca4765ac03e2f5504f66d6ac014bc5ce
junmile <designeryjun@gmail.com>
2020-03-23 21:47:12 +0900

    #1.77

--- src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
+++ src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -18,12 +18,30 @@ const resolvers: Resolvers = {
         const user: User = req.user;
         if (user.isDriving) {
           try {
-            const ride = await Ride.findOne({
-              id: args.rideId,
-              status: 'REQUESTING'
-            });
+            let ride: Ride | undefined;
+            if (args.status === 'ACCEPTED') {
+              ride = await Ride.findOne({
+                id: args.rideId,
+                status: 'REQUESTING'
+              });
+              if (ride) {
+                ride.driver = user;
+                user.isTaken = true;
+                user.save();
+              }
+            } else {
+              ride = await Ride.findOne({
+                id: args.rideId,
+                driver: user
+              });
+            }
             if (ride) {
               ride.status = args.status;
+              ride.save();
+              return {
+                ok: true,
+                error: null
+              };
             } else {
               return {
                 ok: false,
@@ -36,10 +54,12 @@ const resolvers: Resolvers = {
         } else {
           return {
             ok: false,
-            error: 'User is no driving'
+            error: 'You are not driving'
           };
         }
       }
     )
   }
 };
+
+export default resolvers;
