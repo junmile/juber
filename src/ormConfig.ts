e271b1efade865cc79ee23ef81ca1f57bb0e8619
junmile <designeryjun@gmail.com>
2020-07-07 19:57:45 +0900

    heroku finish

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -3,14 +3,14 @@ dotenv.config();
 import { ConnectionOptions } from 'typeorm';
 const connectionOptions: ConnectionOptions = {
   type: 'postgres',
-  database: 'juber',
+  database: process.env.DB_NAME || 'juber',
   synchronize: true,
   logging: false,
   entities: ['entities/**/*.*'],
   host: process.env.DB_ENDPOINT,
   port: 5432,
   username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
 };
 
 export default connectionOptions;
